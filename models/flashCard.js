2360934356b301dc2a9ed976486bb2a9ab41573f
Ethan Degenhardt <ethandeg1996@gmail.com>
2021-05-03 21:07:35 -0500

    flashcard and deck funcs

--- models/flashCard.js
+++ models/flashCard.js
@@ -1,13 +1,32 @@
 const db = require("../db");
-const {BadRequestError} = require("../expressError")
+const { BadRequestError } = require("../expressError")
 class FlashCard {
 
-    static async getByDeckId(id){
-        const results = await db.query(`SELECT * FROM flashcards WHERE deck_id = $1`,[id])
-        if(!results.rows.length) throw new BadRequestError;
+    static async getByDeckId(id) {
+        const results = await db.query(`SELECT * FROM flashcards WHERE deck_id = $1`, [id])
         return results.rows
+    }
+
+    static async createDeck(username, name) {
+        const results = await db.query(
+            `INSERT INTO decks (username, name) VALUES ($1, $2) RETURNING username,name, id`, [username, name]
+        )
+        console.log(results)
+        return results.rows[0]
+    }
+
+    static async createFlashCard(deckId, front, back) {
+        const results = await db.query(
+            `INSERT INTO flashcards (deck_id, front_side, back_side)
+            VALUES 
+            ($1, $2, $3)
+            RETURNING *`, [deckId, front, back]
+        )
+
+        if (!results.rows[0]) throw new BadRequestError;
+        return results.rows[0]
     }
 }
 
 
-module.exports=FlashCard;
+module.exports = FlashCard;
--- routes/flashCards.js
+++ routes/flashCards.js
@@ -2,15 +2,40 @@ const express = require("express");
 const router = new express.Router()
 const FlashCard = require("../models/flashCard")
 
-router.get("/:deck_id", async(req, res,next) => {
+
+router.post("/", async (req, res, next) => {
     try {
-        const {deck_id} = req.params;
-        const response = await FlashCard.getByDeckId(deck_id)
-        return res.json(response)
-    } catch(e){
+        const { frontSide, backSide, deckId } = req.body;
+        const result = await FlashCard.createFlashCard(deckId, frontSide, backSide)
+        return res.status(201).json(result)
+    } catch (e) {
         return next(e)
     }
+
 })
 
+router.post("/deck/new", async (req, res, next) => {
+    try {
+        const { username, name } = req.body;
+        const result = await FlashCard.createDeck(username, name)
+        return res.status(201).json(result)
+    } catch (e) {
+        return next(e)
+    }
 
-module.exports=router;
+})
+
+router.get("/deck/:deck_id", async (req, res, next) => {
+    try {
+        const { deck_id } = req.params;
+        const response = await FlashCard.getByDeckId(deck_id)
+        return res.json(response)
+    } catch (e) {
+        return next(e)
+    }
+});
+
+
+
+
+module.exports = router;
